6643deeebb7613c5fe5022ff24062549cfdd026d
Thomas Petchel <tpetchel@gmail.com>
2021-08-05 22:55:03 -0700

    Initial

---
+++ LearnModule.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+public abstract class MetadataBase : Dictionary<string, object>
+{
+    protected static T Convert<T>(Dictionary<string, object> data) where T : MetadataBase, new()
+    {
+        var t = new T();
+        foreach (var kvp in data)
+        {
+            t[kvp.Key] = kvp.Value;
+        }
+        return t;
+    }
+
+    protected T Lookup<T>(string key) where T : class
+    {
+        if (ContainsKey(key))
+        {
+            return this[key] as T;
+        }
+        return default(T);
+    }
+}
+
+public class ModuleMetadata : MetadataBase
+{
+    public string Uid { get => Lookup<string>("uid"); }
+
+    public string Title { get => Lookup<string>("title"); }
+
+    public string Author { get => Lookup<Dictionary<object, object>>("metadata")["ms.author"].ToString(); }
+
+    public List<string> Products { get => Lookup<List<object>>("products").Select(product => product.ToString()).ToList(); }
+
+    public List<string> Units { get => Lookup<List<object>>("units").Select(unit => unit.ToString()).ToList(); }
+
+    public void Dump()
+    {
+        Dump(this, 0);
+    }
+
+    private static void Dump<TKey>(Dictionary<TKey, object> dictionary, int level)
+    {
+        var indent = new string(' ', 2 * level);
+        foreach (var kvp in dictionary)
+        {
+            if (kvp.Value is Dictionary<object, object>)
+            {
+                Console.WriteLine($"{indent}{kvp.Key}:");
+                Dump(kvp.Value as Dictionary<object, object>, level + 1);
+            }
+            else
+            {
+                Console.WriteLine($"{indent}{kvp.Key}: {kvp.Value.ToString()}");
+            }
+        }
+    }
+
+    public static ModuleMetadata Load(string path)
+    {
+        using (var reader = new StreamReader(path))
+        {
+            var deserializer = new Deserializer();
+            return deserializer.Deserialize<ModuleMetadata>(reader.ReadToEnd());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
+
+// hardcoded; this project is a one-off solution
+var repos = new string[] {
+    "/Users/thpetche/Dev/MicrosoftDocs/learn-pr",
+    "/Users/thpetche/Dev/MicrosoftDocs/learn-m365-pr",
+    "/Users/thpetche/Dev/MicrosoftDocs/learn-dynamics-pr",
+    "/Users/thpetche/Dev/MicrosoftDocs/learn-bizapps-pr",
+    //"/Users/thpetche/Dev/MicrosoftDocs/learn-certs-pr"
+};
+
+using (var workbook = new XLWorkbook())
+{
+    var allMetadata = new List<(string, IEnumerable<ModuleMetadata>)>();
+    foreach (var repo in repos)
+    {
+        string repoName = repo.Split('/').Last();
+        var worksheet = workbook.Worksheets.Add(repoName);
+        var metadataList = CollectModuleMetadata(repo);
+        allMetadata.Add((repoName, metadataList));
+        WriteWorksheet(worksheet, metadataList);
+    }
+
+    var summaryWorksheet = workbook.Worksheets.Add("Summary");
+    WriteSummary(summaryWorksheet, allMetadata);
+
+    workbook.SaveAs("LearnCatalog.xlsx");
+}
+
+static IEnumerable<ModuleMetadata> CollectModuleMetadata(string repo)
+{
+    var metadataList =  new List<ModuleMetadata>();
+    var files = Directory.GetFiles(repo, "index.yml", SearchOption.AllDirectories);
+    foreach (string file in files)
+    {
+        // Skip learning path files.
+        if (file.Contains("learn-pr/paths/"))
+        {
+            continue;
+        }
+        var metadata = ModuleMetadata.Load(file);
+        metadataList.Add(metadata);
+    }
+    return metadataList;
+}
+
+static void WriteWorksheet(IXLWorksheet worksheet, IEnumerable<ModuleMetadata> metadataList)
+{
+    worksheet.Cell(1, 1).Value = "Title";
+    worksheet.Cell(1, 2).Value = "Author";
+    worksheet.Cell(1, 3).Value = "Product";
+
+    var firstRow = worksheet.Row(1);
+    firstRow.Style.Fill.BackgroundColor = XLColor.Black;
+    firstRow.Style.Font.Bold = true;
+    firstRow.Style.Font.FontSize = 16;
+    firstRow.Style.Font.FontColor = XLColor.Aqua;
+    worksheet.SheetView.FreezeRows(1);
+
+    int index = 2;
+    foreach (var metadata in metadataList)
+    {
+        foreach (var product in metadata.Products)
+        {
+            worksheet.Row(index).Style.Font.FontSize = 12;
+            worksheet.Cell(index, 1).Value = metadata.Title;
+            worksheet.Cell(index, 2).Value = metadata.Author;
+            worksheet.Cell(index, 3).Value = product;
+            index++;
+        }
+    }
+
+    foreach (var column in worksheet.ColumnsUsed())
+    {
+        column.AdjustToContents();
+    }
+}
+
+static void WriteSummary(IXLWorksheet worksheet, IEnumerable<(string, IEnumerable<ModuleMetadata>)> allMetadata)
+{
+    worksheet.Cell(1, 1).Value = "Repo";
+    worksheet.Cell(1, 2).Value = "Modules";
+
+    var firstRow = worksheet.Row(1);
+    firstRow.Style.Fill.BackgroundColor = XLColor.Black;
+    firstRow.Style.Font.Bold = true;
+    firstRow.Style.Font.FontSize = 16;
+    firstRow.Style.Font.FontColor = XLColor.Aqua;
+    worksheet.SheetView.FreezeRows(1);
+
+    int index = 2;
+    foreach (var (repo, metadataList) in allMetadata)
+    {
+        worksheet.Row(index).Style.Font.FontSize = 12;
+        worksheet.Cell(index, 1).Value = repo;
+        worksheet.Cell(index, 2).Value = metadataList.Count();
+        index++;
+    }
+
+    foreach (var column in worksheet.ColumnsUsed())
+    {
+        column.AdjustToContents();
+    }
+}
